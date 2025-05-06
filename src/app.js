d787665c0e69670d70c5c5ee833721ee08bd06fb
Valentina Gómez <valengogu96@gmail.com>
2025-05-06 19:27:51 +0000

    terminamos proyecto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronouns = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let textHTML = ""
+  
+  for (let p = 0; p < pronouns.length; p++){
+    for (let a = 0; a < adj.length; a++){
+      for (let n= 0; n < noun.length; n++){
+        textHTML += `<li> ${pronouns[p]}${adj[a]}${noun[n]} </li>`
+      }
+    }
+  }
+  console.log(textHTML)
+  let dominios = document.querySelector("#dominios")
+  dominios.innerHTML = textHTML
+
+}
